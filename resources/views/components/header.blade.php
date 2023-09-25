a5637fc37c844d9f2a6334f2b873b06f9b211378
Dr-Dm <0557586@gmail.com>
2023-09-25 12:13:50 +0300

    update

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -32,7 +32,7 @@
                 <strong>Расписание занятий</strong>
             </a>
 
-            <a href="{{ route('admin.index') }}" class="navbar-brand d-flex align-items-center">
+            <a href="{{ route('backpack.dashboard') }}" class="navbar-brand d-flex align-items-center">
                 {{--                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>--}}
                 <strong>Админка</strong>
             </a>

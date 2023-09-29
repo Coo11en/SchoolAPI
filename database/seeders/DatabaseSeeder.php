527cc75cd659bfd09672b8c7997334aa8411dfd1
coollen <abcfank@yandex.ru>
2023-09-29 03:37:53 +0300

    Added API for information on office equipment, dining room menu, fixed in the album API output of an empty array when requesting a non-existent album (displays 404 error)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,9 @@ use App\Models\Appeals_category;
 use App\Models\Classroom;
 use App\Models\Day;
 use App\Models\Cabinet;
+use App\Models\Menu;
+use App\Models\Menu_basic;
+use App\Models\Menu_item;
 use App\Models\News;
 use App\Models\News_category;
 use App\Models\Parents;
@@ -184,6 +187,28 @@ class DatabaseSeeder extends Seeder
                 'name' => $cabinet,
             ])->create();
 
+        // Создаем 60 новых полей для меню
+        for ($i = 1; $i < 31; $i++) {
+            Menu_basic::factory()->state([
+                'date' => date_date_set(now(), 2023, 9, $i),
+                'name_menu' => 'Завтрак'
+            ])->create();
+            Menu_basic::factory()->state([
+                'date' => date_date_set(now(), (int)date('Y'), (int)date('m'), $i),
+                'name_menu' => 'Обед'
+            ])->create();
+        }
+        // Создаем 50 блюд
+        Menu_item::factory(50)->create();
+        // Создаем 60 меню
+        for ($i = 1; $i < 61; $i++){
+            for ($j = 1; $j < 5; $j++) {
+                Menu::factory()->state([
+                    'menu_basic_id' => Menu_basic::find($i)->id,
+                    'menu_item_id' => Menu_item::all()->random()->id,
+                ])->create();
+            }
+        }
 
 
 

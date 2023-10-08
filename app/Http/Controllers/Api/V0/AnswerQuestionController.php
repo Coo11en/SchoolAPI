c375db05aa55ca336f38823aa25d8df3c3413c0f
coollen <abcfank@yandex.ru>
2023-10-08 13:33:03 +0300

    fix API answer_question

--- app/Http/Controllers/Api/V0/AnswerQuestionController.php
+++ app/Http/Controllers/Api/V0/AnswerQuestionController.php
@@ -12,7 +12,7 @@ class AnswerQuestionController extends Controller
     public function store(Request $request)
     {
         if(Appeal::all()->find($request->input('id')) !== null) {
-            Appeal::all()->firstOrFail('id', '=', $request->input('id'))->update(["date_completion" => now()]);
+            Appeal::all()->firstOrFail('id', '=', $request->input('id'))->update(["response" => $request->input('text'), "status" => true, "date_completion" => now()]);
             return response()->json([
                 'message' => 'Ответ на вопрос записан.',
             ]);
